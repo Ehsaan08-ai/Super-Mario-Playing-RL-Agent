de4184a39236b26b10b20595a99909fd44072548
Ehsaan08-ai <Ehsaanshaikh08@gmail.com>
2026-04-04 20:19:44 +0530

    Initial commit

--- agent.py
+++ agent.py
@@ -1,3 +1,20 @@
 import os
 import torch
 import gym_super_mario_bros
+import yaml
+import torch.nn as nn
+import torch.optim as optim
+import random
+
+if torch.backend.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+class Agent:
+    def run(self, is_training=True, render=False):
+        env = gym_super_mario_bros.make("SuperMarioBros-v0", render_mode="human" if render else "rgb_array")
+        
+        
