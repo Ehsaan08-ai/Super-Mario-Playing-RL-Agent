62724ea7ae32fa3a466f2f43354c9d18b5fae070
Ehsaan08-ai <Ehsaanshaikh08@gmail.com>
2026-04-05 22:30:49 +0530

    Initial commit

--- environment.py
+++ environment.py
@@ -1,4 +1,5 @@
 import os
+import gym
 import random
 import torch
 import numpy as np 
@@ -6,8 +7,10 @@ import gym_super_mario_bros
 import yaml
 import torch.nn as nn
 import torch.optim as optim
-import torch.transforms as transform
-from gym.wrappers import FrameStack, GrayScaleObservation, ResizeObservation 
+import torch.transforms as T 
+from gym.wrappers import FrameStack, GrayScaleObservation, ResizeObservation
+from gym.spaces import Box
+from nes_py.wrappers import JoypadSpace
 
 
 if torch.backend.mps.is_available():
@@ -24,37 +27,48 @@ env = JoypadSpace(env, [["right"], ["right", "A"]])
 class SkipFrame:
     def __init__(self, env, skip):
         super().__init__(env)
-        self._skip = skip
+        self._skip = skip # No. of frames to skip
+
+        self._obs_buffer = np.zeros((2,) + env.observation_space.shape, dtype=np.uint8)
 
     def step(self, action):
-        total_reward = 0.0
-        for i in range(self._skip):
-            obs, reward, done, truncate, info = self.env.step(action)
-            total_reward += reward
-            if done or truncate:
+        total_reward = 0.0 # Starting with 0 reward
+        for i in range(self._skip): # Loop for the number of frames to skip
+            obs, reward, done, truncate, info = self.env.step(action) # This actually moves the mario in the game
+            
+            if i == self._skip - 2:
+                self._obs_buffer[0] = obs
+            elif i == self._skip - 1:
+                self._obs_buffer[1] = obs
+            
+            total_reward += reward # Adding the reward
+            if done or truncate: # If the game is done or truncated
                 break
-        return obs, total_reward, done, truncate, info
+        
+        max_frame = self._obs_buffer.max(axis=0)
+        return max_frame, total_reward, done, truncate, info # Returns the final screen of the skip, the sum of all points
 
 
-class GrayScaleObservation(gym.ObservationWrapper):
+class GrayScaleObservation(gym.ObservationWrapper): # This class turns the game from (RGB) -> Black & White
     def __init__(self, env):
         super().__init__(env)
-        obs_shape = self.observation_space.shape[:2]
-        self.observation_space = Box(
+        obs_shape = self.observation_space.shape[:2] # Getting the ht. & wt. of the screen.
+        self.observation_space = Box( # This line redefines the vision of neural network (from 3 colors to 1 color)
             low=0,
             high=255,
-            shape=obs_shape[0],
+            shape=obs_shape,
             dtype=np.uint8
         ) 
 
     def permute_orientation(self, observation):
         # Convert the [H, W, C]array to [C, H, W]    
         observation =  np.transpose(observation, (2, 0, 1))
-        observation = torch.tensor(observation.copy(), dtype=torch.float32)
+        observation = torch.tensor(observation.copy(), dtype=torch.float32) # converting the image into tensors the format that the NN understands
         return observation
 
-    def observation(self, observation):
+    def observation(self, observation): # The main fnx that processes the img.
         observation = self.permute_orientation(observation)
-        observation = T.Grayscale()
-        observation = transform(observation)
+        observation = T.Grayscale() # This line actually removes the color
+        observation = T(observation) # This line resizes the image
         return observation
+
